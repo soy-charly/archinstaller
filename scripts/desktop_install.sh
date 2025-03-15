60fa807a532dfd74330b568f7d5163cd1b29fe0c
soy-charly <carlos.sanchez.carrasco2008@gmail.com>
2025-03-15 21:17:07 +0100

    Actualizar desktop_install.sh

--- scripts/desktop_install.sh
+++ scripts/desktop_install.sh
@@ -2,6 +2,6 @@
 
 read -p "¿Desea instalar Hyprland y Ly? (s/n): " INSTALL_HYPR
 if [[ "$INSTALL_HYPR" =~ ^[Ss]$ ]]; then
-    arch-chroot /mnt pacman -S --noconfirm hyprland ly nano kitty waybar rofi dunst alacritty grim slurp xdg-desktop-portal-hyprland xdg-user-dirs polkit-kde-agent firefox
+    arch-chroot /mnt pacman -S --noconfirm hyprland ly nano kitty waybar rofi dunst alacritty grim slurp xdg-desktop-portal-hyprland xdg-user-dirs polkit-kde-agent firefox dolphin 
     arch-chroot /mnt systemctl enable ly
 fi
