1c14a7230708457378bade300fe64a1c7ff2591e
soy-charly <carlos.sanchez.carrasco2008@gmail.com>
2025-04-22 22:31:50 +0200

    Update 07_desktop.sh

--- chroot_scripts/07_desktop.sh
+++ chroot_scripts/07_desktop.sh
@@ -1,25 +1,38 @@
 #!/bin/bash
 
-PS3='Elige entorno de escritorio: '
-select opt in GNOME KDE Xfce LXQt Ninguno; do
-    case $opt in
-        GNOME)
+PS3='Elige el número correspondiente al entorno de escritorio: '
+options=("GNOME" "KDE" "Xfce" "LXQt" "Hyprland" "Ninguno")
+select opt in "${options[@]}"; do
+    case $REPLY in
+        1)
+            echo "Instalando GNOME..."
             pacman -Sy --noconfirm gnome gdm
             systemctl enable gdm
             break;;
-        KDE)
+        2)
+            echo "Instalando KDE..."
             pacman -Sy --noconfirm plasma sddm
             systemctl enable sddm
             break;;
-        Xfce)
+        3)
+            echo "Instalando Xfce..."
             pacman -Sy --noconfirm xfce4 lightdm
             systemctl enable lightdm
             break;;
-        LXQt)
+        4)
+            echo "Instalando LXQt..."
             pacman -Sy --noconfirm lxqt sddm
             systemctl enable sddm
             break;;
-        Ninguno) break;;
-        *) echo "Opción inválida";;
+        5)
+            echo "Instalando Hyprland..."
+            pacman -Sy --noconfirm hyprland
+            echo "exec hyprland" >> ~/.xinitrc
+            break;;
+        6)
+            echo "No se instalará ningún entorno de escritorio."
+            break;;
+        *)
+            echo "Opción inválida. Por favor, elige un número entre 1 y ${#options[@]}.";;
     esac
 done
